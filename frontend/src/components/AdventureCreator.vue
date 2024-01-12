e5d77b4e072c56d916ee54bf398e6b97ca753f1b
am <am@am>
2024-01-12 04:06:03 +0300

    added v-stepper for adventure creator

--- frontend/src/components/AdventureCreator.vue
+++ frontend/src/components/AdventureCreator.vue
@@ -1,105 +1,130 @@
+/* eslint-disable */
 <template>
-    <div class="homepage bg-slate-800 h-screen flex justify-center">
-        
-        <div class="w-1/3 flex">
-            <div class="w-full flex flex-col justify-start items-center p-10" v-show="worldPreview">
-                <h3 class="text-3xl  font-bold text-slate-200 text-center">World preview</h3>
-                <img :src="worldPreview" style="object-fit: contain;" class="self-start"/>
-            </div>
-        </div>
-        <div class="w-1/3 flex flex-col gap-8">
-            <div v-if="errors.network">{{ errors.network.message }}</div>
-            <div class="flex items-center justify-start">
-                <router-link to="/" class="link">
-                    <font-awesome-icon icon="fa-solid fa-angle-left" class="text-sky-500 hover:text-sky-700  mr-auto" size="2xl"/>
-                </router-link>
-                <h1 class="mx-auto text-5xl py-10 font-bold text-slate-200 text-center">{{ pageTitle }} your adventure</h1>
-            </div>
-            <div class="w-full">
-                <input type="text" id="name" v-model="adventure.name" class="user-input" placeholder="Give your adventure a name">
-                <span v-if="errors.name" class="error">{{ errors.name }}</span>
-            </div>
-            <div class="w-full">
-                <label for="description" class="text-3xl font-bold text-slate-200">World</label>
-                <Popper placement="right" class="dark">
-                    <template #content>
-                      <div>Give a short description of your world</div>
-                    </template>
-                    <textarea id="description" v-model="adventure.worldDescription" @input="generateWorldPreview" class="user-input"
-                    placeholder="A world where an alien invasion took place. An alien race attacked the Earth. "></textarea>
-                </Popper>
-                <span v-if="errors.description" class="error">{{ errors.description }}</span>
-            </div>
-            <div class="w-full flex flex-col gap-4">
-                <h3 class="text-3xl font-bold text-slate-200">Character</h3>
-                
-                <div>
-                    <h3 class="text-2xl font-bold text-slate-200">Age</h3>
-                    <v-btn-toggle
-                        v-model="adventure.character.age"
-                        divided
-                        color="purple-darken-4"
-                    >
-                        <v-btn v-for="(age, index) in ages" :key="index" :value="age">{{ age }}</v-btn>
-                    </v-btn-toggle>
-                </div>
-                <div>
-                    <h3 class="text-2xl font-bold text-slate-200">Gender</h3>
-                    <v-btn-toggle
-                        v-model="adventure.character.gender"
-                        divided
-                        color="purple-darken-4"
-                    >
-                        <v-btn v-for="(gender, index) in genders" :key="index" :value="gender">{{ gender }}</v-btn>
-                    </v-btn-toggle>
-                </div>
-                <div>
-                    <label for="character_description" class="text-2xl font-bold text-slate-200">Character backstory</label>
-                    <Popper placement="right" class="dark">
-                        <template #content>
-                        <div>Describe your character - who are you? What are you up to? What is your story?</div>
-                        </template>
-                        <textarea id="charscter_description" v-model="adventure.character.description" class="user-input"></textarea>
-                    </Popper>
-                    <span v-if="errors.character_description" class="error">{{ errors.character_description }}</span>
-                    <v-btn @click="generateAvatarPreview">Create avatar</v-btn>
-                </div>
-                
-            </div>
-            <div class="w-full" v-show="adventure.avatarDescription && !generatingAvatarPrompt">
-                <label for="avatar_description" class="text-3xl font-bold text-slate-200">Avatar</label>
-                <Popper placement="right" class="dark">
-                    <template #content>
-                      <div>This is the initial avatar of your character. It will change in the course of the story.</div>
-                    </template>
-                    <input type="text" id="avatar_description" v-model="adventure.avatarDescription" @input="generateAvatar" class="user-input">
-                </Popper>
-                
-                <span v-if="errors.avatar_description" class="error">{{ errors.avatar_description }}</span>
-            </div>
-            <div v-show="!generatingAvatar" class="flex gap-3 items-center justify-center">
-                <button @click.prevent="submitAdventure" :disabled="isSaving" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                {{ btnCaption }}
-            </button>
-            <clip-loader :loading="isSaving" :color="'#3b82f6'" :size="'25px'"></clip-loader>
-            </div>
-        </div>
-        <div class="w-1/3 flex justify-center"> 
-            <div class="w-full flex flex-col justify-start items-center p-10" v-show="avatarPreview">
-                <div class="flex items-center justify-center gap-4">
-                    <h3 class="text-3xl font-bold text-slate-200">Avatar preview</h3>
-                    <font-awesome-icon v-show="!generatingAvatar" icon="fa-solid fa-rotate-right" class="text-sky-500 hover:text-sky-700  mr-auto cursor-pointer" size="2xl" @click="generateAvatar"/>
-                </div>
-                <div :class="['avatar-preview',{ 'with-before': generatingAvatar }]">
-                    <img :src="avatarPreview" style="object-fit: contain;" class="self-start" alt="Avatar preview" />
-                    <clip-loader :loading="generatingAvatar" :color="'#ffffff'" :size="'50px'"></clip-loader>
-                </div>
-                
-            </div>
-        </div>
+    <div class="homepage bg-slate-800 min-h-full justify-center overflow-y-auto">
+        <v-stepper non-linear bg-color="blue-grey-darken-4">
+            <v-stepper-header>
+                <v-stepper-item editable value="1">
+                    World & Character
+                </v-stepper-item>
+
+                <v-divider></v-divider>
+
+                <v-stepper-item editable value="2">World elements</v-stepper-item>
+
+                <v-divider></v-divider>
+
+                <v-stepper-item value="3" editable>Story elements</v-stepper-item>
+
+                <v-divider></v-divider>
+
+                <v-stepper-item value="4" editable>Something</v-stepper-item>
+            </v-stepper-header>
+            <v-stepper-window>
+                <v-stepper-window-item value="1">
+                    <div class="w-1/3 flex">
+                            <div class="w-full flex flex-col justify-start items-center p-10" v-show="worldPreview">
+                                <h3 class="text-3xl  font-bold text-slate-200 text-center">World preview</h3>
+                                <img :src="worldPreview" style="object-fit: contain;" class="self-start"/>
+                            </div>
+                        </div>
+                        <div class="w-1/3 flex flex-col gap-8">
+                            <div v-if="errors.network">{{ errors.network.message }}</div>
+                            <div class="flex items-center justify-start">
+                                <router-link to="/" class="link">
+                                    <font-awesome-icon icon="fa-solid fa-angle-left" class="text-sky-500 hover:text-sky-700  mr-auto" size="2xl"/>
+                                </router-link>
+                                <h1 class="mx-auto text-5xl py-10 font-bold text-slate-200 text-center">{{ pageTitle }} your adventure</h1>
+                            </div>
+                            <div class="w-full">
+                                <input type="text" id="name" v-model="adventure.name" class="user-input" placeholder="Give your adventure a name">
+                                <span v-if="errors.name" class="error">{{ errors.name }}</span>
+                            </div>
+                            <div class="w-full">
+                                <label for="description" class="text-3xl font-bold text-slate-200">World</label>
+                                <Popper placement="right" class="dark">
+                                    <template #content>
+                                    <div>Give a short description of your world</div>
+                                    </template>
+                                    <textarea id="description" spellcheck="disable" v-model="adventure.worldDescription" @input="generateWorldPreview" class="user-input"
+                                    placeholder="A world where an alien invasion took place. An alien race attacked the Earth. "></textarea>
+                                </Popper>
+                                <span v-if="errors.description" class="error">{{ errors.description }}</span>
+                            </div>
+                            <div class="w-full flex flex-col gap-4">
+                                <h3 class="text-3xl font-bold text-slate-200">Character</h3>
+                                
+                                <div>
+                                    <h3 class="text-2xl font-bold text-slate-200">Age</h3>
+                                    <v-btn-toggle
+                                        v-model="adventure.character.age"
+                                        divided
+                                        color="purple-darken-4"
+                                    >
+                                        <v-btn v-for="(age, index) in ages" :key="index" :value="age">{{ age }}</v-btn>
+                                    </v-btn-toggle>
+                                </div>
+                                <div>
+                                    <h3 class="text-2xl font-bold text-slate-200">Gender</h3>
+                                    <v-btn-toggle
+                                        v-model="adventure.character.gender"
+                                        divided
+                                        color="purple-darken-4"
+                                    >
+                                        <v-btn v-for="(gender, index) in genders" :key="index" :value="gender">{{ gender }}</v-btn>
+                                    </v-btn-toggle>
+                                </div>
+                                <div>
+                                    <label for="character_description" class="text-2xl font-bold text-slate-200">Character backstory</label>
+                                    <Popper placement="right" class="dark">
+                                        <template #content>
+                                        <div>Describe your character - who are you? What are you up to? What is your story?</div>
+                                        </template>
+                                        <textarea id="charscter_description" v-model="adventure.character.description" class="user-input"></textarea>
+                                    </Popper>
+                                    <span v-if="errors.character_description" class="error">{{ errors.character_description }}</span>
+                                    <v-btn @click="generateAvatarPreview">Create avatar</v-btn>
+                                </div>
+                                
+                            </div>
+                            <div class="w-full" v-show="adventure.avatarDescription && !generatingAvatarPrompt">
+                                <label for="avatar_description" class="text-3xl font-bold text-slate-200">Avatar</label>
+                                <Popper placement="right" class="dark">
+                                    <template #content>
+                                    <div>This is the initial avatar of your character. It will change in the course of the story.</div>
+                                    </template>
+                                    <input type="text" id="avatar_description" v-model="adventure.avatarDescription" @input="generateAvatar" class="user-input">
+                                </Popper>
+                                
+                                <span v-if="errors.avatar_description" class="error">{{ errors.avatar_description }}</span>
+                            </div>
+                            <div v-show="!generatingAvatar" class="flex gap-3 items-center justify-center">
+                                <button @click.prevent="submitAdventure" :disabled="isSaving" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                {{ btnCaption }}
+                            </button>
+                            <clip-loader :loading="isSaving" :color="'#3b82f6'" :size="'25px'"></clip-loader>
+                            </div>
+                        </div>
+                        <div class="w-1/3 flex justify-center"> 
+                            <div class="w-full flex flex-col justify-start items-center p-10" v-show="avatarPreview">
+                                <div class="flex items-center justify-center gap-4">
+                                    <h3 class="text-3xl font-bold text-slate-200">Avatar preview</h3>
+                                    <font-awesome-icon v-show="!generatingAvatar" icon="fa-solid fa-rotate-right" class="text-sky-500 hover:text-sky-700  mr-auto cursor-pointer" size="2xl" @click="generateAvatar"/>
+                                </div>
+                                <div :class="['avatar-preview',{ 'with-before': generatingAvatar }]">
+                                    <img :src="avatarPreview" style="object-fit: contain;" class="self-start" alt="Avatar preview" />
+                                    <clip-loader :loading="generatingAvatar" :color="'#ffffff'" :size="'50px'"></clip-loader>
+                                </div>
+                                
+                            </div>
+                        </div>
+                </v-stepper-window-item>
+                <v-stepper-window-item value="2">123</v-stepper-window-item>
+                <v-stepper-window-item value="3">123</v-stepper-window-item>
+                <v-stepper-window-item value="4">123</v-stepper-window-item>
+            </v-stepper-window>
+        </v-stepper>
     </div>
   </template>
-
 <script>
     import axios from 'axios';
     import router from '@/router'
@@ -136,7 +161,21 @@
                 current_message: '',
                 generatingAvatar: false,
                 generatingAvatarPrompt: false,
-                defaultAvatar: ''
+                defaultAvatar: '',
+      items: [
+        {
+          title: "Step 1",
+          content: "This is the content for Step 1"
+        },
+        {
+          title: "Step 2",
+          content: "This is the content for Step 2"
+        },
+        {
+          title: "Step 3",
+          content: "This is the content for Step 3"
+        }
+      ]
             };
         },
         methods: {
@@ -358,6 +397,10 @@
     }
     .avatar-preview::before {
         content: "";
+        
+    }
+    .v-sheet {
+        width: 100%;
         
     }
     
